36d891812cb477d4f7f407c517cf6bdfbfd2dcb5
Adam Kim <adamkim@microsoft.com>
2024-09-18 22:00:19 -0700

    Using default theme for light theme

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,4 +1,4 @@
-import { createTheme } from "@fluentui/react";
+import { createTheme, getTheme } from "@fluentui/react";
 import { useEffect, useState } from "react";
 
 export const LIGHT = 'light';
@@ -52,56 +52,31 @@ export const useTheme = () => {
 };
 
 
-export const webLightTheme = createTheme({
-    palette: {
-      themePrimary: '#a6d400',
-      themeLighterAlt: '#fbfdf3',
-      themeLighter: '#eff8d0',
-      themeLight: '#e2f2a9',
-      themeTertiary: '#c7e55c',
-      themeSecondary: '#b0d91a',
-      themeDarkAlt: '#95be00',
-      themeDark: '#7ea100',
-      themeDarker: '#5d7700',
-      neutralLighterAlt: '#faf9f8',
-      neutralLighter: '#f3f2f1',
-      neutralLight: '#edebe9',
-      neutralQuaternaryAlt: '#e1dfdd',
-      neutralQuaternary: '#d0d0d0',
-      neutralTertiaryAlt: '#c8c6c4',
-      neutralTertiary: '#a19f9d',
-      neutralSecondary: '#605e5c',
-      neutralSecondaryAlt: '#8a8886',
-      neutralPrimaryAlt: '#3b3a39',
-      neutralPrimary: '#323130',
-      neutralDark: '#201f1e',
-      black: '#000000',
-      white: '#ffffff',
-    }});
+export const webLightTheme = getTheme();
 
-    export const webDarkTheme = createTheme({
-        palette: {
-          themePrimary: '#a6d400',
-          themeLighterAlt: '#fbfdf3',
-          themeLighter: '#eff8d0',
-          themeLight: '#e2f2a9',
-          themeTertiary: '#c7e55c',
-          themeSecondary: '#b0d91a',
-          themeDarkAlt: '#95be00',
-          themeDark: '#7ea100',
-          themeDarker: '#5d7700',
-          neutralLighterAlt: '#525252',
-          neutralLighter: '#595959',
-          neutralLight: '#646464',
-          neutralQuaternaryAlt: '#6b6b6b',
-          neutralQuaternary: '#717171',
-          neutralTertiaryAlt: '#898989',
-          neutralTertiary: '#463421',
-          neutralSecondary: '#8b6743',
-          neutralSecondaryAlt: '#8b6743',
-          neutralPrimaryAlt: '#cc9762',
-          neutralPrimary: '#e8ac6f',
-          neutralDark: '#eebf90',
-          black: '#f2cfac',
-          white: '#4a4a4a',
-        }});
+export const webDarkTheme = createTheme({
+    palette: {
+        themePrimary: '#a6d400',
+        themeLighterAlt: '#fbfdf3',
+        themeLighter: '#eff8d0',
+        themeLight: '#e2f2a9',
+        themeTertiary: '#c7e55c',
+        themeSecondary: '#b0d91a',
+        themeDarkAlt: '#95be00',
+        themeDark: '#7ea100',
+        themeDarker: '#5d7700',
+        neutralLighterAlt: '#525252',
+        neutralLighter: '#595959',
+        neutralLight: '#646464',
+        neutralQuaternaryAlt: '#6b6b6b',
+        neutralQuaternary: '#717171',
+        neutralTertiaryAlt: '#898989',
+        neutralTertiary: '#463421',
+        neutralSecondary: '#8b6743',
+        neutralSecondaryAlt: '#8b6743',
+        neutralPrimaryAlt: '#cc9762',
+        neutralPrimary: '#e8ac6f',
+        neutralDark: '#eebf90',
+        black: '#f2cfac',
+        white: '#4a4a4a',
+    }});
